028ef7836250f4407d9e94bc6fde44943127e3da
Sandbox Sync <support@getsandbox.com>
2016-12-01 13:57:10 +0000

    Changes

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ Sandbox.define('/text-xml','GET', function(req, res) {
         // return res.send(400, 'Invalid content type, expected text/xml not "' + ctype + '"');
         res.type('application/json');
         res.status(400);
-        return res.json(req.headers['Content-Type']);
+        return res.json({ type: req.headers['Content-Type'] });
     }
     
     // Set the type of response, sets the content type.
